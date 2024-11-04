b1022e892a60dcf443579fd510dc67b4a9d1a322
Hosea <karanja.mungai23@gmail.com>
2024-11-04 14:34:01 +0300

    modifies react components

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,32 @@ function App() {
 
   return (
     <div className="App">
-      <iframe
-        width="919"
-        height="525"
+      <iframe  
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         frameBorder="0"
         allowFullScreen
         title="Thinking in React"
       />
+      <h2>{video.title}</h2>
+      <div id="info">
+        <p style={{ borderRight: "2px solid black",  paddingRight: "10px"}}>Views: {video.views.toLocaleString()}</p>
+        <p style={{paddingLeft: "10px"}} >Uploaded: {video.createdAt}</p>        
+      </div>
+      <div>
+        <button>{video.upvotes} 👍</button>
+        <button>{video.downvotes} 👎</button>
+      </div>
+      <button id="hideComments">Hide Comments</button>
+      <hr></hr>
+      <h3>{video.comments.length} Comments</h3>
+      {video.comments.map((comment)=>{
+        return(
+          <div key={comment.id}>
+            <h4>{comment.user}</h4>
+            <p>{comment.comment}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
